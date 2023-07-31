34696b317be780f3fd670da75589860a4d4fb644
= <=>
2023-07-31 19:01:47 +0300

    Massive changes to form and variable names. Trying to figure out what data race calculations need.

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,25 +1,37 @@
 import React, { useEffect, useState } from "react";
 
-function Form() {
+const Form = ({ setFormData }) => {
   const [trackName, setTrackName] = useState("");
   const [trackLength, setTrackLength] = useState("");
   const [laps, setLaps] = useState("");
-  const [fuelLap, setFuelLap] = useState("");
-  const [fuelMax, setFuelMax] = useState("");
+  const [normalFuelLap, setNormalFuelLap] = useState("");
+  const [attackFuelLap, setAttackFuelLap] = useState("");
+  const [fuelLoadMax, setFuelLoadMax] = useState("");
+  const [fuelStart, setFuelStart] = useState("");
+  const [normalLapTime, setNormalLapTime] = useState("");
+  const [attackLapTime, setAttackLapTime] = useState("");
+  const [normalTyreLife, setNormalTyreLife] = useState("");
+  const [attackTyreLife, setAttackTyreLife] = useState("");
+  const [timeLoss, setTimeLoss] = useState("");
+  const [drivers, setDrivers] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(
-      "Track: " +
-        trackName +
-        "\nLength: " +
-        trackLength +
-        "\nLaps: " +
-        laps +
-        "\nFuel/lap: " +
-        fuelLap +
-        "\nMax Fuel: " +
-        fuelMax
-    );
+    let newVal = {
+      trackName: trackName,
+      trackLength: trackLength,
+      laps: laps,
+      normalFuelLap: normalFuelLap,
+      attackFuelLap: attackFuelLap,
+      fuelLoadMax: fuelLoadMax,
+      normalLapTime: normalLapTime,
+      attackLapTime: attackLapTime,
+      normalTyreLife: normalTyreLife,
+      attackTyreLife: attackTyreLife,
+      timeLoss: timeLoss,
+      drivers: drivers,
+    };
+    setFormData(newVal);
   };
 
   return (
@@ -56,12 +68,22 @@ function Form() {
           </td>
         </tr>
         <tr>
-          <td>Fuel Consuption/Lap:</td>
+          <td>Normal Fuel Consuption/Lap:</td>
           <td>
             <input
               type="text"
-              value={fuelLap}
-              onChange={(e) => setFuelLap(e.target.value)}
+              value={normalFuelLap}
+              onChange={(e) => setNormalFuelLap(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Attack Fuel Consuption/Lap:</td>
+          <td>
+            <input
+              type="text"
+              value={attackFuelLap}
+              onChange={(e) => setAttackFuelLap(e.target.value)}
             />
           </td>
         </tr>
@@ -70,8 +92,80 @@ function Form() {
           <td>
             <input
               type="text"
-              value={fuelMax}
-              onChange={(e) => setFuelMax(e.target.value)}
+              value={fuelLoadMax}
+              onChange={(e) => setFuelLoadMax(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Start Fuel Load:</td>
+          <td>
+            <input
+              type="text"
+              value={fuelStart}
+              onChange={(e) => setFuelStart(e.target.value)}
+              disabled
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Normal laptime:</td>
+          <td>
+            <input
+              type="text"
+              value={normalLapTime}
+              onChange={(e) => setNormalLapTime(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Attack laptime:</td>
+          <td>
+            <input
+              type="text"
+              value={attackLapTime}
+              onChange={(e) => setAttackLapTime(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Refuel time loss:</td>
+          <td>
+            <input
+              type="text"
+              value={timeLoss}
+              onChange={(e) => setTimeLoss(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Normal tyre life:</td>
+          <td>
+            <input
+              type="text"
+              value={normalTyreLife}
+              onChange={(e) => setNormalTyreLife(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Attack tyre life:</td>
+          <td>
+            <input
+              type="text"
+              value={attackTyreLife}
+              onChange={(e) => setAttackTyreLife(e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Number of drivers:</td>
+          <td>
+            <input
+              type="text"
+              value={drivers}
+              onChange={(e) => setDrivers(e.target.value)}
+              disabled
             />
           </td>
         </tr>
@@ -84,6 +178,6 @@ function Form() {
       </table>
     </form>
   );
-}
+};
 
 export default Form;
