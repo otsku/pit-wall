aacaf587293d38198c1b8c645c940c38b26b178f
= <=>
2023-07-31 19:02:53 +0300

    Created some functions that handle race calculations

--- src/components/functions.js
+++ src/components/functions.js
@@ -1,2 +1,19 @@
-function Functions() {}
-export default Functions;
+export const getRaceLength = (formData) => {
+  return formData.trackLength * formData.laps;
+};
+
+export const getFuelConsumption = (formData) => {
+  return formData.normalFuelLap * formData.laps;
+};
+
+export const getRefuels = (formData) => {
+  return (getFuelConsumption(formData) / formData.fuelLoadMax) | 0;
+};
+
+export const getPitTime = (formData) => {
+  return formData.timeLoss * getRefuels(formData);
+};
+
+export const getMaxStint = (formData) => {
+  return (formData.fuelLoadMax / formData.normalFuelLap) | 0;
+};
